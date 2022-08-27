68077e515b3bf8f570c219f2e25f1a0f4e719809
Adrian Trujillo <adrian.td96@gmail.com>
2022-08-27 13:31:10 -0500

    added helper function for unweighted algorithms

---
+++ src/algorithms/unweighted/getNeighbors.ts
@@ -0,0 +1,13 @@
+import { NodeType, GridType } from '../../types/index';
+
+export const getNeighbors = (node: NodeType, grid: GridType) => {
+  const neighbors: NodeType[] = [];
+  const { row, col } = node;
+
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+
+  return neighbors;
+};
